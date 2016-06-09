03b2fcb4930037c92638425c8a917252c5aa4119
vrunoa <vruno@urucas.com>
2016-06-08 20:52:21 -0400

    fix status comparing

--- bot.sh
+++ bot.sh
@@ -8,13 +8,13 @@ function push {
   echo $RESPONSE  
 }
 
-MYSQL_STATUS=$(service mysql status | grep "running")
-if [ -z MYSQL_STATUS ]; then
+MYSQL_STATUS=$(service mysql status | grep "running"; echo $?)
+if [ "$MYSQL_STATUS" -eq "1" ]; then
   push "WOOOT! MySQL service has stopped, quickly connect that service up again! ...or move to mongo, what ever you want."
 fi;
 
-APACHE_STATUS=$(service apache2 status | grep "is running")
-if [ -z APACHE_STATUS ]; then
+APACHE_STATUS=$(service apache2 status | grep "is running"; echo $?)
+if [ "$APACHE_STATUS" -eq "1" ]; then
   push "WOOOT! Apache2 is not running, your pages are unreachebles! Ohhh, won't somebody please think of the children."
 fi;
 
